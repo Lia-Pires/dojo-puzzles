8d7d9038241d26df2d8e21aab0e399a1d8be9878
Lia Pires <liasth.pires@gmail.com>
2023-04-15 13:52:05 -0300

    3rd sollution

--- Dojo_puzzles/diamantes/diamantes.py
+++ Dojo_puzzles/diamantes/diamantes.py
@@ -1,78 +1,11 @@
-"""
-Dado uma letra ('A' a 'Z'), exiba um diamante iniciando em 'A' e tendo a letra 
-fornecida com o ponto mais distante.
-                                                                                
-Por exemplo, dado a letra 'E' temos:
+from diamantes_funcoes import (
+    base_diamante,
+    topo_diamante,
+    filtrar_alfabeto,
+    inverter_lista,
+)
 
-    A   
-
-   B B
-
-  C   C
-
- D     D
-
-E       E 
-
- D     D 
-
-  C   C
-
-   B B
-
-    A
-
- 
-
-Dado a letra 'C' temos:
-
-  A
-
- B B
-
-C   C
-
- B B
-
-  A
-
-"""
-
-import string
-
-alfabeto = list(string.ascii_uppercase)
 letra_digitada = input(f'Digite uma letra do alfaberto diferente de "A": ').upper()
 
-espaco = " "
-espaco_inicio = alfabeto.index(letra_digitada) + 1
-espaco_meio = 0
-lista_ate_letra_digitada = [
-    letra
-    for letra in alfabeto
-    if alfabeto.index(letra) <= alfabeto.index(letra_digitada)
-]
-
-for letra in lista_ate_letra_digitada:
-    espaco_inicio -= 1
-
-    if letra == "A":
-        print(f"{espaco * espaco_inicio}{letra}")
-    else:
-        espaco_meio += 2
-        print(f"{espaco * espaco_inicio}{letra}{espaco * espaco_meio}{letra}")
-    print()
-
-lista_invertida = list(reversed(lista_ate_letra_digitada))
-espaco_inicio = 1
-
-for letra in lista_invertida:
-    if lista_invertida.index(letra) != 0 and letra != "A":
-        espaco_meio -= 2
-        print(f"{espaco * espaco_inicio}{letra}{espaco * espaco_meio}{letra}")
-        print()
-        espaco_inicio += 1
-    if letra == "A":
-        print(f"{espaco * espaco_inicio}{letra}")
-
-
-# alfabeto.index(letra_digitada.upper())
+topo_diamante(filtrar_alfabeto(letra_digitada), letra_digitada)
+base_diamante(inverter_lista(filtrar_alfabeto(letra_digitada)))
---
+++ Dojo_puzzles/diamantes/diamantes_funcoes.py
@@ -0,0 +1,94 @@
+"""
+Dado uma letra ('A' a 'Z'), exiba um diamante iniciando em 'A' e tendo a letra 
+fornecida com o ponto mais distante.
+                                                                                
+Por exemplo, dado a letra 'E' temos:
+
+    A   
+
+   B B
+
+  C   C
+
+ D     D
+
+E       E 
+
+ D     D 
+
+  C   C
+
+   B B
+
+    A
+
+ 
+
+Dado a letra 'C' temos:
+
+  A
+
+ B B
+
+C   C
+
+ B B
+
+  A
+
+"""
+
+import string
+
+espaco = " "
+espaco_meio = 0
+
+
+def filtrar_alfabeto(letra_digitada: str) -> list:
+    """Retorna uma lista do alfabeto até a letra recebida"""
+    alfabeto = list(string.ascii_uppercase)
+    lista_filtrada = [
+        letra
+        for letra in alfabeto
+        if alfabeto.index(letra) <= alfabeto.index(letra_digitada)
+    ]
+    return lista_filtrada
+
+
+def inverter_lista(lista: list) -> list:
+    """Recebe uma lista e retorna a mesma lista invertida"""
+    lista_invertida = list(reversed(lista))
+    return lista_invertida
+
+
+def topo_diamante(lista_filtrada: list, letra_digitada: list) -> None:
+    """Gera o topo do diamante de letras recebendo a
+    lista já filtrada e a letra que foi escolhida"""
+    global espaco_meio
+    espaco_inicio = lista_filtrada.index(letra_digitada) + 1
+    for letra in lista_filtrada:
+        espaco_inicio -= 1
+        if letra:
+            if letra == "A":
+                print(f"{espaco * espaco_inicio}{letra}")
+            else:
+                espaco_meio += 2
+                print(f"{espaco * espaco_inicio}{letra}{espaco * espaco_meio}{letra}")
+            print()
+
+
+def base_diamante(lista_invertida: list) -> None:
+    """Gera a base do diamante de letra recebendo a lista
+    que foi usada na função topo_diamante, porém invertida"""
+    global espaco_meio
+    espaco_inicio = 1
+    for letra in lista_invertida:
+        if lista_invertida.index(letra) != 0 and letra != "A":
+            espaco_meio -= 2
+            print(f"{espaco * espaco_inicio}{letra}{espaco * espaco_meio}{letra}")
+            print()
+            espaco_inicio += 1
+        if letra == "A":
+            print(f"{espaco * espaco_inicio}{letra}")
+
+
