757058bd1025ff81e64001cca77a1bf3297984f0
Lia Pires <liasth.pires@gmail.com>
2023-04-15 11:54:30 -0300

    1st sollution

--- Dojo_puzzles/diamantes/diamantes.py
+++ Dojo_puzzles/diamantes/diamantes.py
@@ -40,29 +40,47 @@ C   C
 
 import string
 
-
 alfabeto = list(string.ascii_uppercase)
-letra_digitada = input(f"Digite uma letra qualquer: ").upper()
+# letra_digitada = input(f"Digite uma letra qualquer: ").upper()
+letra_digitada = "Z"
 
 espaco = " "
 espacos = alfabeto.index(letra_digitada) + 1
 letra_anterior = None
+lista_ate_digitada = [
+    letra
+    for letra in alfabeto
+    if alfabeto.index(letra) <= alfabeto.index(letra_digitada)
+]
 
-for letra in alfabeto:
+for letra in lista_ate_digitada:
     espacos -= 1
-    if alfabeto.index(letra) <= alfabeto.index(letra_digitada):
-        if letra == "A":
-            print(f"{espaco * espacos}{letra}")
-            letra_anterior = letra
-        else:
-            meio = (
-                (alfabeto.index(letra_anterior) + alfabeto.index(letra))
-                if letra_anterior in alfabeto
-                else espacos
-            )
-            print(f"{espaco * espacos}{letra}{espaco * meio}{letra}")
-            letra_anterior = letra
+
+    if letra == "A":
+        print(f"{espaco * espacos}{letra}")
+        letra_anterior = letra
+    else:
+        meio = (
+            (lista_ate_digitada.index(letra_anterior) + lista_ate_digitada.index(letra))
+            if letra_anterior in lista_ate_digitada
+            else espacos
+        )
+        print(f"{espaco * espacos}{letra}{espaco * meio}{letra}")
+        letra_anterior = letra
     print()
+
+lista_invertida = list(reversed(lista_ate_digitada))
+espacos = alfabeto.index(letra_digitada) + 1
+espacos = 1
+
+for letra in lista_invertida:
+    if lista_invertida.index(letra) != 0 and letra != "A":
+        meio -= 2
+        print(f"{espaco * espacos}{letra}{espaco * meio}{letra}")
+        print()
+        espacos += 1
+    if letra == "A":
+        print(f"{espaco * espacos}{letra}")
 
 
 # alfabeto.index(letra_digitada.upper())
