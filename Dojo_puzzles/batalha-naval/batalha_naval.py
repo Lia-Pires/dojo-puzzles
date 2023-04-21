6510ce15f462a9a9de35d937d123996f4686ba0c
Lia Pires <liasth.pires@gmail.com>
2023-04-21 17:21:36 -0300

    feat: jogo completo batalha naval

---
+++ Dojo_puzzles/batalha-naval/batalha_naval.py
@@ -0,0 +1,7 @@
+from batalha_naval_funcoes import Jogo
+
+nome_1 = input("Digite o nome do jogador 1: ")
+nome_2 = input("Digite o nome do jogador 2: ")
+
+
+Jogo.jogo(nome_1, nome_2)
--- Dojo_puzzles/batalha-naval/batalha_naval_funcoes.py
+++ Dojo_puzzles/batalha-naval/batalha_naval_funcoes.py
@@ -29,24 +29,24 @@ dos jogadores.
 from typing import Type
 
 
-# Em andamento
-class Tabuleiro:
-    def __init__(self):
-        A = [0, 1, 2, 3, 4]
-        B = [0, 1, 2, 3, 4]
-        C = [0, 1, 2, 3, 4]
-        D = [0, 1, 2, 3, 4]
-        E = [0, 1, "a", 3, 4]
+class Jogo:
+    navios = {
+        "p.a": "Porta-Aviões",
+        "enc": "Encouraçado",
+        "sub": "Submarino",
+        "des": "Destroyer",
+        "b.p": "Barco de Patrulha",
+    }
 
+    def __init__(self, nome):
+        A = ["b.p", 0, 0, 0, 0]
+        B = [0, 0, 0, 0, 0]
+        C = [0, 0, 0, 0, 0]
+        D = [0, 0, 0, 0, 0]
+        E = [0, 0, 0, 0, 0]
+
+        self.nome = nome
         self.tabuleiro = [A, B, C, D, E]
-
-        navios = (
-            {"p.a": "Porta-Aviões"},
-            {"enc": "Encouraçado"},
-            {"sub": "Submarino"},
-            {"des": "Destroyer"},
-            {"bp": "Barco de Patrulha"},
-        )
 
     # TODO: colocar o tipo de jogador nas annotations
     def verificar_espacos(lista: list, jogador) -> bool:
@@ -73,7 +73,7 @@ class Tabuleiro:
     ):
         lista = [[num1, num2], [num3, num4], [num5, num6], [num7, num8], [num9, num10]]
 
-        if Tabuleiro.verificar_espacos(lista, jogador):
+        if Jogo.verificar_espacos(lista, jogador):
             for itens in lista:
                 jogador.tabuleiro[itens[0]][itens[1]] = "p.a"
 
@@ -90,7 +90,7 @@ class Tabuleiro:
     ):
         lista = [[num1, num2], [num3, num4], [num5, num6], [num7, num8]]
 
-        if Tabuleiro.verificar_espacos(lista, jogador):
+        if Jogo.verificar_espacos(lista, jogador):
             for itens in lista:
                 jogador.tabuleiro[itens[0]][itens[1]] = "enc"
 
@@ -99,7 +99,7 @@ class Tabuleiro:
     ):
         lista = [[num1, num2], [num3, num4], [num5, num6]]
 
-        if Tabuleiro.verificar_espacos(lista, jogador):
+        if Jogo.verificar_espacos(lista, jogador):
             for itens in lista:
                 jogador.tabuleiro[itens[0]][itens[1]] = "sub"
 
@@ -108,71 +108,76 @@ class Tabuleiro:
     ):
         lista = [[num1, num2], [num3, num4], [num5, num6]]
 
-        if Tabuleiro.verificar_espacos(lista, jogador):
+        if Jogo.verificar_espacos(lista, jogador):
             for itens in lista:
                 jogador.tabuleiro[itens[0]][itens[1]] = "des"
 
     def posicionar_barco_patrulha(jogador, num1: int, num2: int, num3: int, num4: int):
         lista = [[num1, num2], [num3, num4]]
 
-        if Tabuleiro.verificar_espacos(lista, jogador):
+        if Jogo.verificar_espacos(lista, jogador):
             for itens in lista:
                 jogador.tabuleiro[itens[0]][itens[1]] = "b.p"
 
     def organizar_tabuleiro(jogador):
         print(
-            "Digite os os índices de um tabuleiro 10x10 onde quer posicionar suas peças (LxC)!"
+            f"{jogador.nome} digite os os índices de um tabuleiro 10x10 onde quer posicionar suas peças (LxC)!\n"
         )
-        print("Tabuleiro inicial: ")
+        print("Tabuleiro inicial: \n")
+        ind = 0
+        print("  0, 1, 2, 3, 4")
         for indice in jogador.tabuleiro:
-            print(indice)
+            print(f"{ind}{indice}")
+            ind += 1
 
         num1, num2, num3, num4, num5, num6, num7, num8, num9, num10 = map(
             int, input("Porta aviões, comprimento 5: ").split(",")
         )
-        Tabuleiro.posicionar_porta_avioes(
+        Jogo.posicionar_porta_avioes(
             jogador, num1, num2, num3, num4, num5, num6, num7, num8, num9, num10
         )
 
-        num1, num2, num3, num4, num5, num6, num7, num8, num9, num10 = map(
+        num1, num2, num3, num4, num5, num6, num7, num8 = map(
             int, input("Encouraçado, comprimento 4: ").split(",")
         )
-        Tabuleiro.posicionar_encouracado(
+        Jogo.posicionar_encouracado(
             jogador, num1, num2, num3, num4, num5, num6, num7, num8
         )
 
-        num1, num2, num3, num4, num5, num6, num7, num8, num9, num10 = map(
+        num1, num2, num3, num4, num5, num6 = map(
             int, input("Destroyer, comprimento 3: ").split(",")
         )
-        Tabuleiro.posicionar_destroyer(jogador, num1, num2, num3, num4, num5, num6)
+        Jogo.posicionar_destroyer(jogador, num1, num2, num3, num4, num5, num6)
 
-        um1, num2, num3, num4, num5, num6, num7, num8, num9, num10 = map(
+        num1, num2, num3, num4, num5, num6 = map(
             int, input("Submarino, comprimento 3: ").split(",")
         )
-        Tabuleiro.posicionar_submarino(jogador, num1, num2, num3, num4, num5, num6)
+        Jogo.posicionar_submarino(jogador, num1, num2, num3, num4, num5, num6)
 
-        num1, num2, num3, num4, num5, num6, num7, num8, num9, num10 = map(
+        num1, num2, num3, num4 = map(
             int, input("Barco Patrulha, comprimento 2: ").split(",")
         )
-        Tabuleiro.posicionar_barco_patrulha(jogador, num1, num2, num3, num4)
+        Jogo.posicionar_barco_patrulha(jogador, num1, num2, num3, num4)
 
         print("Tabuleiro Final: ")
         for indice in jogador.tabuleiro:
             print(indice)
+        print()
 
     def tabuleiro_oponente(oponente):
         print("Escolha o índice onde quer atacas (LxC):")
         print("  0, 1, 2, 3, 4")
         ind = 0
-        for indice in jogador_1.tabuleiro:
+        for indice in oponente.tabuleiro:
             print(f"{ind} [*, *, *, *, *]")
             ind += 1
 
     def atacar_oponente(oponente, num1, num2):
         if type(oponente.tabuleiro[num1][num2]) != int:
-            print(f"Você atingiu uma parte do {oponente.tabuleiro[num1][num2]}")
             atingido = oponente.tabuleiro[num1][num2]
+            print(f"Você atingiu uma parte do {Jogo.navios.get(atingido)}")
             oponente.tabuleiro[num1][num2] = 0
+            Jogo.verificar_navio_afundado(atingido, oponente)
         else:
             print("Você não atingiu seu oponente, aguarde a próxima rodada.")
 
@@ -180,22 +185,60 @@ class Tabuleiro:
         for lista in jogador.tabuleiro:
             fim = all(isinstance(item, int) for item in lista)
             if fim == False:
-                return False, print("A partida continua, próxima rodada!")
+                return False
             else:
                 pass
-        return True, print("A partida chegou ao fim")
+        return True
 
     def verificar_navio_afundado(atingido, oponente):
         navio = any(atingido in sublista for sublista in oponente.tabuleiro)
         if navio:
             pass
         else:
-            navio_afundado = Tabuleiro.navios.get("atingido")
+            navio_afundado = Jogo.navios.get(atingido)
             print(f"Você afundou o {navio_afundado}")
 
+    def jogo(nome1, nome2):
+        jogador_1 = Jogo(nome1)
+        jogador_2 = Jogo(nome2)
 
-jogador_1 = Tabuleiro()
-jogador_2 = Tabuleiro()
+        Jogo.organizar_tabuleiro(jogador_1)
+        Jogo.organizar_tabuleiro(jogador_2)
 
+        fim = Jogo.verificar_fim_partida(jogador_2)
+        fim2 = Jogo.verificar_fim_partida(jogador_1)
 
-Tabuleiro.verificar_fim_partida(jogador_1)
+        while not (fim or fim2):
+            print("A Partida Continua, próxima rodada. ")
+            print(f"Vez do {jogador_1.nome}")
+            Jogo.tabuleiro_oponente(jogador_2)
+            num1 = int(input("Linha: "))
+            num2 = int(input("Coluna: "))
+            Jogo.atacar_oponente(jogador_2, num1, num2)
+            fim2 = Jogo.verificar_fim_partida(jogador_2)
+            if fim2:
+                print(f"A partida chegou ao fim!, {jogador_1.nome} venceu!")
+                break
+
+            print(f"Vez do {jogador_2.nome}")
+            Jogo.tabuleiro_oponente(jogador_1)
+            num1 = int(input("Linha: "))
+            num2 = int(input("Coluna: "))
+            Jogo.atacar_oponente(jogador_1, num1, num2)
+            fim = Jogo.verificar_fim_partida(jogador_1)
+            if fim:
+                print(f"A partida chegou ao fim!, {jogador_2.nome} venceu!")
+                break
+
+        print("Final: ")
+        print(f"Jogador 1: {jogador_1.nome}")
+        print("  0, 1, 2, 3, 4")
+        for indice in jogador_1.tabuleiro:
+            print(f"{ind}{indice}")
+            ind += 1
+
+        print(f"Jogador 2: {jogador_2.nome}")
+        print("  0, 1, 2, 3, 4")
+        for indice in jogador_2.tabuleiro:
+            print(f"{ind}{indice}")
+            ind += 1
