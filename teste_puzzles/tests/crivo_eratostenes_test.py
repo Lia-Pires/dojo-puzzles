f323539e0b2d55fcf0a556f3328bb3c7619713a9
Lia Pires <liasth.pires@gmail.com>
2023-05-27 22:01:25 -0300

    fix: test renaming folders

--- teste_puzzles/tests/crivo_eratostenes_test.py
+++ teste_puzzles/tests/crivo_eratostenes_test.py
@@ -1,7 +1,7 @@
 import pytest
 from pytest import mark
 
-from dojo_puzzles.crivo_eratostenes.crivo_eratostenes import primos_range
+from teste_puzzles.crivo_eratostenes.crivo_eratostenes import primos_range
 
 
 @mark.parametrize(
